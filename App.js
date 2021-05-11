3fff249148bfd79da5098490f8be521af96945d7
Sohrat Jumadurdyyev <sohrat6128@gmail.com>
2021-05-11 22:46:32 +0300

    create Auth container

--- App.js
+++ App.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import * as React from 'react';
 import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
+import Auth from './src/containers/Auth/Auth';
+
+const Stack = createStackNavigator();
 
 const App = () => {
-  return <View>
-    <Text>App.js</Text>
-  </View>
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen 
+          options={{
+            headerShown: false
+          }} 
+          name="Auth" 
+          component={Auth} 
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 };
 
 export default App;
---
+++ src/containers/Auth/Auth.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text, TouchableOpacity } from 'react-native';
+import SImage from 'react-native-scalable-image';
+
+import styles from './style';
+
+const Auth = () => {
+    return <View>
+        
+    </View>;
+};
+
+export default Auth;
