81918c0e04db7d49d692204acdaeacf505ade16b
Sohrat Jumadurdyyev <sohrat6128@gmail.com>
2021-05-15 14:28:30 +0300

    create OpenUrlButton component

---
+++ src/components/UI/OpenUrlButton/OpenUrlButton.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { TouchableOpacity, Text, Linking, Alert } from 'react-native';
+
+import styles from './style';
+
+const OpenUrlButton = ({url, children}) => {
+    const urlHandle = async () => {
+        const supported = await Linking.canOpenURL(url);
+        if(supported) Linking.openURL(url);
+        else {
+            Alert.alert(
+                'hoppoooooo',
+                'bad url'
+            );
+        };
+    };
+    return (
+        <TouchableOpacity onPress={urlHandle}>
+            <Text>{children}</Text>
+        </TouchableOpacity>
+    );
+};
+
+export default OpenUrlButton;
